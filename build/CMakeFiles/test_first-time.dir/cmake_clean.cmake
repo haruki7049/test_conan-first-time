1e9f5e105def23973a2319879a78bf137e893475
haruki7049 <tontonkirikiri@gmail.com>
2023-03-27 12:00:10 +0900

    first commit

---
+++ build/CMakeFiles/test_first-time.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_first-time.dir/src/main.cpp.o"
+  "CMakeFiles/test_first-time.dir/src/main.cpp.o.d"
+  "bin/test_first-time"
+  "bin/test_first-time.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_first-time.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
